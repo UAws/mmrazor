6e2d91a7ed2614416908054eb311af10c3701581
Akide Liu <liyang.liu@student.adelaide.edu.au>
2022-06-19 03:12:44 +0930

    🧐 fix : path

--- tools/mmseg/dist_train.sh
+++ tools/mmseg/dist_train.sh
@@ -14,6 +14,6 @@ python -m torch.distributed.launch \
     --master_addr=$MASTER_ADDR \
     --nproc_per_node=$GPUS \
     --master_port=$PORT \
-    $(dirname "$0")/mmseg/train_mmseg.py \
+    $(dirname "$0")/train_mmseg.py \
     $CONFIG \
     --launcher pytorch ${@:3}
